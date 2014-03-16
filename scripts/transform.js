931d22a35da5eaf724b10d117ccb06e5e2add31b
Devon Govett <devongovett@gmail.com>
2014-03-16 01:19:05 -0700

    Setup npm scripts

--- scripts/transform.js
+++ scripts/transform.js
@@ -2,10 +2,10 @@
 
 var fs = require('fs');
 
-fs.readdirSync(__dirname + '/ui').forEach(function(file) {
+fs.readdirSync(__dirname + '/../ui').forEach(function(file) {
   if (!/\.js$/.test(file)) return;
 
-  var filename = __dirname + '/ui/' + file;
+  var filename = __dirname + '/../ui/' + file;
   var contents = fs.readFileSync(filename, 'utf8');
   var prepend = ["var jQuery = require('jquery');"];
 
@@ -13,12 +13,12 @@ fs.readdirSync(__dirname + '/ui').forEach(function(file) {
   var deps = contents.match(/\s*\/* Depends:\s*\n(?:[\s\*]*(jquery\.ui\..+\.js)\s*\n)+/);
   if (deps) {
     deps[0].split('\n').slice(1, -1).forEach(function(dep) {
-      dep = dep.replace(/[\s\*]/g, '').replace(/^jquery-ui\./, '').replace(/\.js$/, '');
+      dep = dep.replace(/[\s\*]/g, '').replace(/^jquery[.-]ui\.(.+)\.js/, '$1');
       prepend.push("require('./" + dep + "');");
     });
   }
 
   // prepend jQuery require and all dependencies for the module
-  contents = contents.replace(/^(.|\n)*?\/\*!/, prepend.join('\n') + '\n\n/*!');
-  fs.writeFileSync(filename /*__dirname + '/ui/' + file.replace(/jquery\.ui\./, '')*/, contents, 'utf8');
+  contents = prepend.join('\n') + '\n\n' + contents;
+  fs.writeFileSync(__dirname + '/../' + file.replace(/^jquery[.-]ui\.(.+)\.js/, '$1.js'), contents, 'utf8');
 });
