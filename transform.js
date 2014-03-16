1791aa2fec54c4582517405920825be18a28624a
Devon Govett <devongovett@gmail.com>
2014-03-16 00:25:23 -0700

    Replace shell script with node script to parse dependencies for modules

---
+++ transform.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+
+fs.readdirSync(__dirname + '/ui').forEach(function(file) {
+  if (!/\.js$/.test(file)) return;
+
+  var filename = __dirname + '/ui/' + file;
+  var contents = fs.readFileSync(filename, 'utf8');
+  var prepend = ["var jQuery = require('jQuery');"];
+
+  // parse dependencies in comments
+  var deps = contents.match(/\s*\/* Depends:\s*\n(?:[\s\*]*(jquery\.ui\..+\.js)\s*\n)+/);
+  if (deps) {
+    deps[0].split('\n').slice(1, -1).forEach(function(dep) {
+      dep = dep.replace(/[\s\*]/g, '').replace(/\.js$/, '');
+      prepend.push("require('./" + dep + "');");
+    });
+  }
+
+  // prepend jQuery require and all dependencies for the module
+  contents = contents.replace(/^(.|\n)*\/\*!/, prepend.join('\n') + '\n\n/*!');
+  fs.writeFileSync(filename, contents, 'utf8');
+});
