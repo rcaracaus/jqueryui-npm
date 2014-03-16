66d954a39940263595575e597e19161c9f63f144
Devon Govett <devongovett@gmail.com>
2014-03-16 01:07:07 -0700

    Fixes

--- transform.js
+++ transform.js
@@ -13,12 +13,12 @@ fs.readdirSync(__dirname + '/ui').forEach(function(file) {
   var deps = contents.match(/\s*\/* Depends:\s*\n(?:[\s\*]*(jquery\.ui\..+\.js)\s*\n)+/);
   if (deps) {
     deps[0].split('\n').slice(1, -1).forEach(function(dep) {
-      dep = dep.replace(/[\s\*]/g, '').replace(/\.js$/, '');
+      dep = dep.replace(/[\s\*]/g, '').replace(/^jquery-ui\./, '').replace(/\.js$/, '');
       prepend.push("require('./" + dep + "');");
     });
   }
 
   // prepend jQuery require and all dependencies for the module
-  contents = contents.replace(/^(.|\n)*\/\*!/, prepend.join('\n') + '\n\n/*!');
-  fs.writeFileSync(filename, contents, 'utf8');
+  contents = contents.replace(/^(.|\n)*?\/\*!/, prepend.join('\n') + '\n\n/*!');
+  fs.writeFileSync(filename /*__dirname + '/ui/' + file.replace(/jquery\.ui\./, '')*/, contents, 'utf8');
 });
